c409424222de087a32c88ab24e030f18ec889335
Tryo Asnafi <tryoasnafi@gmail.com>
2020-12-23 00:20:44 +0700

    first commit

---
+++ 1_hello_world.dart
@@ -0,0 +1,7 @@
+void main() {
+  // Fungsi print hanya bisa dipassing 1 positional argument
+  print('Hello World!');
+
+  // Error: Too many positional arguments: 1 expected, but 2 found.
+  // print("Hello", "World!");
+}
---
+++ 2_variable.dart
@@ -0,0 +1,41 @@
+void main() {
+  // Deklarasi variabel dengan tipe data tertentu.
+  // Tipe data di dart => int, double, num, bool, String, List, Map, dynamic
+  String name = "Tryo Asnafi";
+  print(name);
+
+  int age = 19;
+  print(age);
+
+  double weight = 48.2;
+  print(weight);
+
+  // Tipe data num bisa berisi bilangan bulat maupun pecahan
+  num luasLingkaran = 20;
+  print(luasLingkaran);
+
+  bool isStudent = true;
+  print(isStudent);
+
+  List<String> socialMedia = ['Facebook', 'Instagram', 'Twitter', 'LinkedIn'];
+  print(socialMedia);
+  print(socialMedia.runtimeType);
+
+  List listRandom = ['Aku', 'Saya', 2, 4.10, true]; // dynamic
+  print(listRandom);
+  print(listRandom.runtimeType);
+
+  dynamic she = 'Shy';
+  she = 7;
+  print(she);
+
+  // Variabel type inference, assign tanpa menentukan tipe data;
+  var suku = "Minang";
+  print(suku.runtimeType);
+
+  // Deklarasi variabel, otomatis bertipe data dynamic
+  var x;
+  x = "Random Word";
+  x = 2020;
+  print(x);
+}
